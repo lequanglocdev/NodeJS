80182ce95e972500acad5c0735f2b8feaf9c4b30
lequanglocdev <quanglocdev@gmail.com>
2023-11-24 21:38:48 +0700

    CRUD - create

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index'
+import CRUDServices from '../services/CRUDServices'
 let getHomePage = async(req,res) =>{
     try {
         let data = await db.User.findAll()
@@ -17,13 +18,15 @@ let getHomePage = async(req,res) =>{
 let getCRUD = (req,res) =>{
     return res.render('crud.ejs')
 }
+
 // Object:{
 //     key:'',
 //     value:''
 // }
 
-let postCRUD = (req,res) =>{
-    console.log(req.body)
+let postCRUD = async(req,res) =>{
+   const message =  await CRUDServices.createNewUser(req.body)
+   console.log(message)
     return res.send('post crud')
 }
 module.exports ={
